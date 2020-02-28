42c1035000d3cd4d879c2cec064072551da56a0c
Callum <callum@pc-71-55.customer.ask4.lan>
2020-02-28 18:04:10 +0100

    CP5 Finished and Ready

--- CP5.py
+++ CP5.py
@@ -4,41 +4,164 @@ CP5 - Orbital Motion
 # modules required for our simulation
 import matplotlib.pyplot as plt
 import numpy as np
+import math
+import csv
 from matplotlib.animation import FuncAnimation
 
 class Simulation(object):
+    # Creation of necessary Class Variables
+    G = 6.67e-11
+    DaySec = 24.0*60*60
+    
+    def __init__(self, myFileA, myFileB):
+        # constructor, pulls relevant input parameters into the class
+        
+        # reading csv files
+        with open(myFileA, 'r') as file:
+            data_list_a = list(csv.reader(file))
+
+        with open(myFileB, 'r') as file:
+            data_list_b = list(csv.reader(file))
+        
+        # Mars
+        self.xa = float((data_list_a[0])[0])
+        self.ya = float((data_list_a[1])[0])
+        
+        self.xva = float((data_list_a[2])[0])
+        self.yva = float((data_list_a[3])[0])
+        
+        self.ma = float((data_list_a[4])[0])
+        
+        '''
+        self.xa = 0
+        self.ya = 0
+        
+        self.xva = 0
+        self.yva = 0
+        
+        self.ma = 6.4185e23
+        '''
+        
+        # Phobos
+        self.xb = float((data_list_b[0])[0])
+        self.yb = float((data_list_b[1])[0])
+        
+        self.xvb = float((data_list_b[2])[0])
+        self.yvb = math.sqrt((Simulation.G * self.ma)/(self.xb)) # v_{2} = \sqrt{(G * m_{1})/(r_{12})}
+        
+        self.mb = float((data_list_b[4])[0])
+        
+        '''
+        self.orb_radi = 9.3773e6 # orbital radius r_{12}
+        self.xb = self.orb_radi
+        self.yb = 0
+        
+        self.xvb = 0
+        self.yvb = math.sqrt((Simulation.G * self.ma)/(self.xb)) # v_{2} = \sqrt{(G * m_{1})/(r_{12})}
+        
+        self.mb = 1.06e16
+        '''
+        
+        # Object Position Lists
+        self.xalist = []
+        self.yalist = []
+        
+        self.xblist = []
+        self.yblist = []
+        
+    def TotalKE(self):
+        # equation for Total KE
+        # the sum of individual objects kinetic energies, with KE = 1/2 * m * v^{2}
+        # and so v^{2} term calculated using pythagoras of x and y velocities, so square
+        # root squared is just the underlying components
+        return ((0.5 * self.ma * (self.xva**2 + self.yva**2)) + (0.5 * self.mb * (self.xvb**2 + self.yvb**2)))
+        
     def runSim(self):
-        # np.linspace - return evenly spaced numbers over a specified interval
-        theta = np.linspace(0, 2*np.pi, 500)
-        # self.[x/y]pos are arrays where each element has had the [cos/sin] function applied to it
-        self.xpos = np.cos(theta)
-        self.ypos = np.sin(theta)
+        # method to run the simulation of the objects
+        
+        t = 0.0
+        dt = 0.001 * Simulation.DaySec # timestep value
+        
+        # counter to track number of iterations, so as to print Total KE at suitable intervals (roughly at every 10% of iterations)
+        count = 0
+        
+        # loop
+        while t < Simulation.DaySec: # orbital period is apparently 0.319 *DaySec, but we'll roll with this for now
+            # Compute force
+            rx = self.xb - self.xa
+            ry = self.yb - self.ya
             
+            # modr3 being the denominator of our equation
+            modr3 = (rx**2 + ry**2)**1.5
+        
+            fx = -(Simulation.G * self.ma * self.mb * rx)/(modr3)
+            fy = -(Simulation.G * self.ma * self.mb * ry)/(modr3)
+            
+            # Update the quantities:
+            # Phobos
+            self.xvb += (fx*dt)/self.mb
+            self.yvb += (fy*dt)/self.mb
+            
+            self.xb += self.xvb*dt
+            self.yb += self.yvb*dt
+
+            # Mars
+            self.xva += -(fx*dt)/self.ma
+            self.yva += -(fy*dt)/self.ma
+            
+            self.xa += self.xva*dt
+            self.ya += self.yva*dt
+            
+            # incriment t to move simulation forward
+            t += dt
+            
+            # Save positions in lists, these lists being for use in Animation classes methods
+            self.xalist.append(self.xa)
+            self.yalist.append(self.ya)
+            
+            self.xblist.append(self.xb)
+            self.yblist.append(self.yb)
+            
+            count += 1
+            if count % 100 == 0:
+                print("Total Kinetic Energy: " + str(self.TotalKE()) + " J")
+   
+      
 class Animation(object):
-    def __init__(self, myXpos, myYpos):
+    def __init__(self, myXapos, myYapos, myXbpos, myYbpos):
         # constructor, pulls relevant inputs into the class
-        self.xpos = myXpos
-        self.ypos = myYpos
+        self.xapos = myXapos
+        self.yapos = myYapos
+        self.xbpos = myXbpos
+        self.ybpos = myYbpos
 
     def animate(self, i):
-        # update position of the circle
-        self.patch.center = (self.xpos[i], self.ypos[i])
-        return self.patch,
-    
+        # create 2 to be animated, using parameter i to move their position, each with individual arrays
+        self.patches[0].center = (self.xapos[i], self.yapos[i])
+        self.patches[1].center = (self.xbpos[i], self.ybpos[i])
+        return self.patches
+        
     def display(self):
         fig = plt.figure()
         ax = plt.axes()
         
-        # create circle to be animated and add to plot at starting position
-        self.patch = plt.Circle((self.xpos[0], self.ypos[0]), 0.1, color='b', animated=True)
-        ax.add_patch(self.patch)
+        # create an empty list for circles
+        self.patches = []
+        
+        # create circles centred at initial position and then append them to the list
+        self.patches.append(plt.Circle((self.xapos[0], self.yapos[0]), 250000, color='orange', animated=True))
+        self.patches.append(plt.Circle((self.xbpos[0], self.ybpos[0]), 250000, color='thistle', animated=True))
+        
+        # add circles to axes
+        for i in range(0, len(self.patches)):
+            ax.add_patch(self.patches[i])
         
         ax.axis('scaled')
-        ax.set_xlim(-1.5, 1.5)
-        ax.set_ylim(-1.5, 1.5)
+        ax.set_xlim(-10000000, 10000000)
+        ax.set_ylim(-10000000, 10000000)
         
         # animate the plot
-        numFrames = len(self.xpos)
+        numFrames = len(self.xapos)
         
         # fig - the figure object we draw upon
         # self.animate - the function to call at each frame, to generate the next position of things
@@ -48,16 +171,17 @@ class Animation(object):
         # blit=True - blitting algorithm
         anim = FuncAnimation(fig, self.animate, numFrames, repeat=True, interval=20, blit=True)
         
-        plt.show()
-        
-        
+        plt.show()  
+   
+'''     
 def main():
     # Simulation
-    s = Simulation()
+    s = Simulation('mars.csv', 'phobos.csv')
     s.runSim()
     
     # Animation
-    a = Animation(s.xpos, s.ypos)
+    a = Animation(s.xalist, s.yalist, s.xblist, s.yblist)
     a.display()
     
 main()
+'''
---
+++ CP5Test.py
@@ -0,0 +1,16 @@
+from CP5 import Simulation, Animation
+
+class OrbitalMotionTest(object):
+    def run(self):
+        s = Simulation('mars.csv', 'phobos.csv')
+        s.runSim()
+        
+        a = Animation(s.xalist, s.yalist, s.xblist, s.yblist)
+        a.display()
+    
+        
+def main():
+    omt = OrbitalMotionTest()
+    omt.run()
+    
+main()
